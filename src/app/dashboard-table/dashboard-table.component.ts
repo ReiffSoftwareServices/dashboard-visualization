a0397fb489f5ae75d7daf320ca2c808137c898d0
jreiff <janreiff@web.de>
2020-03-10 13:45:30 +0100

    Setup of Angular App with Table

--- src/app/dashboard-table/dashboard-table.component.ts
+++ src/app/dashboard-table/dashboard-table.component.ts
@@ -9,7 +9,7 @@ export interface PeriodicElement {
 
 const ELEMENT_DATA: PeriodicElement[] = [
   {position: 1, name: 'Test Erfolgreich!!', weight: 1.0079, symbol: 'H'},
-  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
+  {position: 2, name: 'Local', weight: 4.0026, symbol: 'He'},
   {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
   {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
   {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
